37b8c5a75ff9ae0aeb9c0a489060dcc6aa5880b0
SaiChappidi <55330680+SaiChappidi@users.noreply.github.com>
2019-10-19 11:00:25 -0400

    Add files via upload

---
+++ Chapter 5.py
@@ -0,0 +1,26 @@
+#money = 2000
+#if money > 1000:
+#    print("I'm rich!!")
+#else:
+#    print("I'm not rich!!")
+#    print("But I might be later...")
+
+#twinkies = 600
+#if twinkies < 100:
+#    print("Too few")
+#elif twinkies>500:
+#    print("Too many")
+
+#money = 1001
+#if money 100<money<500:
+#    print("Between 100 and 500")
+#elif money 1000<money<5000:
+#    print("Between 1000 and 5000")
+
+ninja=5
+if 30<ninja<50:
+    print("Too many")
+elif 10<ninja<30:
+    print("It'll be a struggle but I can take them")
+elif ninja<10:
+    print("I can fight those ninjas")
---
+++ Chapter 6.py
@@ -0,0 +1,23 @@
+# for x in range(0, 20):
+#    print('hello %s' % x)
+#    if x<9:
+#        break
+
+# x=1
+# while x<= 13:
+#    print(x)
+#    x=x+2
+
+#x = 1
+#ingredients = ['snails', 'leeches', 'gorilla belly-button lint', 'caterpillar eyebrows', 'centipede toes']
+#for i in ingredients:
+#    print(x, i)
+#    x = x + 1
+#x=0
+#weight=0
+#for weight in range(1,21):
+#    weight = 122+x
+#    weight=weight*.165
+#    print(x, weight)
+
+#    x=x+1
