cbb6d7a35124d7abacacf43111a87fa100b6e7ab
SaiChappidi <gamersai9@gmail.com>
2019-11-02 17:39:53 -0400

    Teaching

--- org/talon/learning/Python Book/chapter9.py
+++ org/talon/learning/Python Book/chapter9.py
@@ -12,6 +12,7 @@ message = 'this if is you not are a reading very this good then way you to have 
 words = message.split()
 for x in range(0, len(words), 2):
     print(words[x])
+#
 
 
 f = open('test.txt')
